580623e7e429ee8f479daa91814738ee4b7b538d
ExoHyb <theo.martin22@icloud.com>
2016-04-21 12:01:30 +0200

    Création de la maquette du site des assises en PHP. Requiert Bower et SASS.

---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Assises nationales de la médiation numérique</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
+        <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap-theme.min.css" media="screen" title="no title" charset="utf-8">
+        <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css" media="screen" title="no title" charset="utf-8">
+        <link rel="stylesheet" href="css/style.css" media="screen" title="no title" charset="utf-8">
+        <link href='https://fonts.googleapis.com/css?family=Jura' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        <header class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <img class="img-responsive img-header" src="img/banniere.png" alt="" />
+                </div>
+                <div class="col-md-4">
+                    <ul id="navigation-principal">
+                        <li id="element-1"><a href="#">Acceuil</a></li>
+                        <li id="element-2"><a href="#">Programmes</a></li>
+                        <li id="element-3"><a href="#">Partenaires</a></li>
+                        <li id="element-4"><a href="#">Portrait</a></li>
+                        <li id="element-5"><a href="#">Infos Pratiques</a></li>
+                        <li id="element-6"><a href="#">Editions Précédentes</a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
