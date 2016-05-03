20b7cffee14b671cb71f61a84c307230c2503b0a
vniati <niativirgil@gmail.com>
2016-05-03 11:46:54 +0200

    contact / map

--- infos_pratiques.php
+++ infos_pratiques.php
@@ -1,2 +1,21 @@
 <?php include "header.php"; ?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-9">
+          <div class="contenu-principal">
+            <h2>Les diffèrents lieux</h2>
+            <iframe width="100%" height="300px" frameBorder="0" src="https://umap.openstreetmap.fr/fr/map/assises-nationales-de-la-mediation-numerique_83695?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=true&datalayersControl=true&onLoadPanel=undefined&captionBar=false"></iframe><p><a href="https://umap.openstreetmap.fr/fr/map/assises-nationales-de-la-mediation-numerique_83695">Voir en plein écran</a></p>
+          </div>
+          <div class="div_form contenu-principal">
+            <h2>Contact</h2>
+            <?php include "contact_form.php"; ?>
+          </div>
+
+        </div>
+        <div class="col-md-3">
+            <?php include "sidebar.php"; ?>
+        </div>
+    </div>
+</div>
+
 <?php include "footer.php"; ?>
