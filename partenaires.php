3052942c909fb75ce98242cc83fe270b48b6a2fc
delseny anne <anne.delseny@gmail.com>
2016-05-04 09:28:03 +0200

    fichier partenaires en cours

--- partenaires.php
+++ partenaires.php
@@ -1,2 +1,172 @@
 <?php include "header.php"; ?>
+
+  <section class="partner-content">
+
+    <div class="row">
+      <div class="col-md-12">
+      <h1> Des partenaires engagés...</h1>
+    </div>
+  </div>
+
+     <div class="row">
+       <div class="col-md-12">
+       <h3> Partenaires locaux</h3>
+     </div>
+   </div>
+
+     <div class="row">
+       <div class="col-md-3">
+         <a href="http://www.cnfpt.fr/content/delegation-regionale-basse-normandie?gl=ZmRhYTVhYzc"></a>
+         <img src="img/1441225690.png" alt="">
+       </div>
+
+       <div class="col-md-3">
+         <a href="http://www.mairie-deauville.fr/"></a>
+           <img src="img/1441225707.png" alt="">
+       </div>
+
+       <div class="col-md-3">
+          <a href="http://www.le-far.fr/"></a>
+          <img src="img/1441225914.png" alt="">
+       </div>
+
+       <div class="col-md-3">
+          <a href="http://www.crij-haute-normandie.org/"></a>
+          <img src="img/1441225932.jpg" alt="">
+       </div>
+     </div>
+
+     <div class="row">
+       <div class="col-md-3">
+          <a href="http://banditvisions.com/"></a>
+          <img src="img/1441226328.jpg" alt="">
+       </div>
+
+       <div class="col-md-3">
+          <a href="http://caen.fr/"></a>
+          <img src="img/image1.jpg" alt="">
+       </div>
+
+       <div class="col-md-3">
+          <a href="http://www.infojeunesse.fr/http://www.supinfo.com/"></a>
+          <img src="img/image 1.png" alt="">
+       </div>
+
+       <div class="col-md-3">
+          <a href="http://www.relais-sciences.org/"></a>
+          <img src="img/image 2.jpg" alt="">
+       </div>
+     </div>
+
+     <div class="row">
+       <div class="col-md-3">
+          <a href="http://www.supinfo.com/"></a>
+          <img src="img/image 2.png" alt="">
+       </div>
+
+       <div class="col-md-3">
+          <a href="http://www.culturecommunication.gouv.fr/Regions/Drac-Normandie"></a>
+          <img src="img/image.jpg" alt="">
+       </div>
+
+       <div class="col-md-3">
+          <a href="http://www.manchenumerique.fr/"></a>
+          <img src="img/image.png" alt="">
+       </div>
+
+       <div class="col-md-3">
+          <a href="http://www.laliguebn.org/lebn4/"></a>
+          <img src="img/image 3.jpg" alt="">
+       </div>
+     </div>
+
+     <div class="row">
+       <div class="col-md-3">
+          <a href="http://casusbelli.fr/"></a>
+          <img src="img/image 4.jpg" alt="">
+       </div>
+
+       <div class="col-md-3">
+          <a href="https://www.ac-caen.fr/"></a>
+          <img src="img/image 5.jpg" alt="">
+       </div>
+
+       <div class="col-md-3">
+          <a href="http://www.culturecommunication.gouv.fr/Regions/Drac-Normandie"></a>
+          <img src="img/image 6.jpg" alt="">
+       </div>
+
+       <div class="col-md-3">
+          <a href="http://www.saint-lo-agglo.fr/"></a>
+          <img src="img/image 7.jpg" alt="">
+       </div>
+     </div>
+
+     <div class="row">
+       <div class="col-md-3">
+          <a href="https://www.facebook.com/lacodingschool"></a>
+          <img src="img/image 8.jpg" alt="">
+       </div>
+
+       <div class="col-md-3">
+          <a href="http://www.ctexdev.net/"></a>
+          <img src="img/image 9.jpg" alt="">
+       </div>
+
+       <div class="col-md-3">
+          <a href="https://www.ac-caen.fr/politique-educative/soutien-et-accompagnement-aux-projets-des-equipe/education-aux-medias-clemi/"></a>
+          <img src="img/image 12.jpg" alt="">
+       </div>
+
+       <div class="col-md-3">
+          <a href="http://www2.crdp.ac-caen.fr/web/guest/alaune"></a>
+          <img src="img/image 11.jpg" alt="">
+       </div>
+     </div>
+
+     <div class="row">
+       <div class="col-md-3">
+          <a href="http://www.caf.fr/ma-caf/caf-du-calvados/actualites"></a>
+          <img src="img/image 13.jpg" alt="">
+       </div>
+
+       <div class="col-md-3">
+          <a href="http://www.novimage.org/home.htm"></a>
+          <img src="img/image 14.jpg" alt="">
+       </div>
+
+       <div class="col-md-3">
+          <a href="http://www.onisep.fr/Mes-infos-regionales/Basse-Normandie"></a>
+          <img src="img/image 15.jpg" alt="">
+       </div>
+
+       <div class="col-md-3">
+          <a href="https://www.caf.fr/ma-caf/caf-de-la-manche/actualites"></a>
+          <img src="img/image 16.jpg" alt="">
+       </div>
+     </div>
+
+     <div class="row">
+       <div class="col-md-3">
+          <a href="https://yousign.fr/"></a>
+          <img src="img/image 17.jpg" alt="">
+       </div>
+
+       <div class="col-md-3">
+          <a href="https://image.jimcdn.com/app/cms/image/transf/dimension=146x10000:format=jpg/path/s6aad603de09dcbc7/image/i724f713deb16a211/version/1443904509/image.jpg"></a>
+          <img src="img/image 18.jpg" alt="">
+       </div>
+
+       <div class="col-md-3">
+          <a href="https://makeidea.io/"></a>
+          <img src="img/image 19.jpg" alt="">
+       </div>
+
+       <div class="col-md-3">
+          <a href="http://www.cultureetnature.com/"></a>
+          <img src="img/image 20.jpg" alt="">
+       </div>
+     </div>
+
+  </section>
 <?php include "footer.php"; ?>
